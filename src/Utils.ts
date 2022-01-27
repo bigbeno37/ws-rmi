26961d990075687a6de638b222c96d324e64f36a
bigbeno37 <contact@benosullivan.com>
2022-01-28 00:28:33 +1100

    Added documentation to Utils.ts

--- src/Utils.ts
+++ src/Utils.ts
@@ -6,4 +6,10 @@
  */
 export const xor = (condition1: boolean, condition2: boolean) => !condition1 !== !condition2;
 
-export const isEqualToAny = <T>(obj: unknown, ...compareTo: T[]) => compareTo.some(item => obj === item);
+/**
+ * Returns whether the given value is equal to any of the items in compareTo.
+ *
+ * @param value The value to compare.
+ * @param compareTo The values to be compared against value.
+ */
+export const isEqualToAny = <T>(value: unknown, ...compareTo: T[]) => compareTo.some(item => value === item);
